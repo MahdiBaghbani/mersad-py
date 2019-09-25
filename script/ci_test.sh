46df0bf8a1e7d9e1b2e49c935f070bbc4cadd553
Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>
2019-09-25 11:13:50 +0330

    ci: code coverage with travis.
    Signed-off-by: Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>

--- script/ci_test.sh
+++ script/ci_test.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
-
+#
 # set flag for shell execution
 # -e  Exit immediately if a command exits with a non-zero status.
 # -x  Print commands and their arguments as they are executed.
 set -ex
 
-script/test_style.sh
-script/test_unittest.sh
+# run style tests against flake8
+script/test_flake8.sh
+# run style tests against pylint
+script/test_pylint.sh
+# run static type checks
+script/test_mypy.sh
+# test and test coverage
+script/test_uittest.sh
