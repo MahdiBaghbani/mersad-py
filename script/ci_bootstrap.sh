f11874aa75545f4b7bb3e99dc3a535878d3a1dc0
Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>
2019-09-25 20:14:34 +0330

    ci: last commit message was definitely a big lie.
    Signed-off-by: Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>

--- script/ci_bootstrap.sh
+++ script/ci_bootstrap.sh
@@ -13,7 +13,10 @@ set -ex
 apt-get update -y
 # install python 3.6
 apt-get install -y python3.6
-# verify installation
+# verify installation path
 whereis python3
+# set alias for python3.6 > python3
+alias python3='usr/bin/python3.6'
+# verify python3 command works, and version of python is 3.6
 python3 -V
 command -v curl
