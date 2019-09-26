4e84db0059acbbacb768f14de40f13de38819f3e
Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>
2019-09-26 15:39:51 +0330

    add: version module to hold package's version.
    Signed-off-by: Mohammad Mahdi Baghbani Pourvahid <Mahdi.baghbani1@gmail.com>

--- setup.py
+++ setup.py
@@ -1,6 +1,21 @@
+# Python Standard Library
+import re
+
 # 3rd Party Library
 from setuptools import find_packages
 from setuptools import setup
+
+# load version
+VERSION_FILE = "mersad/_version.py"
+VERSION_STRING = open(VERSION_FILE, "r").read()
+REGEX_PATTERN = r"^__version__ = ['\"]([^'\"]*)['\"]"
+
+re_search = re.search(REGEX_PATTERN, VERSION_STRING, re.M)
+
+if re_search:
+    version = re_search.group(1)
+else:
+    raise RuntimeError(f"Unable to find version string in {VERSION_FILE}.")
 
 # general information about package
 name = "mersad"
@@ -9,6 +24,18 @@ license_name = "AGPLv3"
 author = "Mohammad Mahdi Baghbani Pourvahid"
 author_email = "MahdiBaghbani@protonmail.com"
 description = """Azadeh Afzar - mersad Cryptographic Library"""
+classifiers = [
+    "Development Status :: 1 - Planning",
+    "Environment :: Console",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: GNU Affero General Public License v3",
+    "License :: OSI Approved :: zlib/libpng License",
+    "Topic :: Software Development",
+    "Topic :: Utilities",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7"
+]
 
 # filter test directory from packages
 packages = {package for package in find_packages() if "test" not in package}
@@ -33,7 +60,7 @@ install_requires = ["ErfanIO"]
 
 setup(
         name=name,
-        version="0.0.1",
+        version=version,
         packages=packages,
         package_data=package_data,
         url=url,
@@ -41,5 +68,6 @@ setup(
         author=author,
         author_email=author_email,
         description=description,
+        classifiers=classifiers,
         install_requires=install_requires
 )
